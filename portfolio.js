7ce2af0262cd7bb2fb41c3cdd98201151003d58e
Myeongsoo Kim <wardballoon@gmail.com>
2019-04-15 21:37:15 +0900

    Portfolio 애니메이션 업데이트

--- portfolio.js
+++ portfolio.js
@@ -1,82 +1,3 @@
-/* HEADER */
-window.onload = function() {scrollFunction()};
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.documentElement.scrollTop > 70) {
-      var header = document.getElementById('header');
-      if(!header.classList.contains('navbar-fixed')){
-        header.classList.add('navbar-fixed');
-        document.getElementsByTagName('body')[0].style.marginTop = '70px';
-        header.style.display = 'none';
-        setTimeout(function() {
-          header.style.display = 'block';
-        }, 40);
-      }
-    } else {
-      var header = document.getElementById('header');
-      if(header.classList.contains('navbar-fixed')){
-        header.classList.remove('navbar-fixed');
-        document.getElementsByTagName('body')[0].style.marginTop = '0';
-      }
-    }
-}
-
-function menuToggle(){
-  document.getElementById('menu').classList.toggle('show');
-}
-
-document.getElementById('toggleBtn').addEventListener('click', menuToggle);
-
-/* WELCOME AREA */
-var imageSlideIndex = 1;
-showImageSlides(imageSlideIndex);
-
-function imageSlideTimer() {
-  plusImageSlides(1);
-}
-
-var imageTimer = setInterval(imageSlideTimer, 3000);
-
-function plusImageSlides(n) {
-  clearInterval(imageTimer);
-  imageTimer = setInterval(imageSlideTimer, 3000);
-
-  showImageSlides(imageSlideIndex += n);
-}
-
-function currentImageSlide(n){
-  clearInterval(imageTimer);
-  imageTimer = setInterval(imageSlideTimer, 3000);
-
-  showImageSlides(imageSlideIndex = n);
-}
-
-function showImageSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName('image-slide');
-  var dots = document.getElementsByClassName('dot');
-  if (n > slides.length) {imageSlideIndex = 1}
-  if (n < 1) {imageSlideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = 'none';
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(' active', '');
-  }
-  slides[imageSlideIndex-1].style.display = 'block';
-  dots[imageSlideIndex-1].className += ' active';
-}
-
-document.getElementById('imagePrev').addEventListener('click', plusImageSlides.bind(null,-1));
-document.getElementById('imageNext').addEventListener('click', plusImageSlides.bind(null,1));
-
-document.getElementById('firstDot').addEventListener('click', currentImageSlide.bind(null,1));
-document.getElementById('secondDot').addEventListener('click', currentImageSlide.bind(null,2));
-document.getElementById('thirdDot').addEventListener('click', currentImageSlide.bind(null,3));
-document.getElementById('forthDot').addEventListener('click', currentImageSlide.bind(null,4));
-
-
 /* PORTFOLIO AREA */
 filterSelection('all');
 
@@ -111,7 +32,7 @@ function removeClass(element, name) {
  while(arr.indexOf(name) > -1){
    arr.splice(arr.indexOf(name), 1);
  }
-
+ /* 배열의 원소들을 연결하여 하나의 값으로 만듭니다. */
  element.className = arr.join(" ");
 }
 
@@ -150,21 +71,3 @@ var filterItems = document.getElementsByClassName('overlay');
 for(var i=0;i<filterItems.length;i++){
   filterItems[i].addEventListener('click', viewPortfolio);
 }
-
-
-
-/* ANCHOR */
-function moveTo(id) {
-  if(id == 'brand'){
-    window.scrollTo(0, 0);
-  } else {
-    window.scrollTo(0, document.getElementById(id).offsetTop - 70);
-  }
-  document.getElementById('menu').classList.remove('show');
-}
-
-document.getElementById('navbarBrand').addEventListener('click', moveTo.bind(null,'brand'));
-document.getElementById('navbarAbout').addEventListener('click', moveTo.bind(null,'about'));
-document.getElementById('navbarService').addEventListener('click', moveTo.bind(null,'service'));
-document.getElementById('navbarPortfolio').addEventListener('click', moveTo.bind(null,'portfolio'));
-document.getElementById('navbarReview').addEventListener('click', moveTo.bind(null,'review'));
